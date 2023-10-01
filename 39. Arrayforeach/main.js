62365285e8a2d40c57d66aa26b01be98e24fba22
rafael <rafaelsumanti01@gmail.com>
2023-10-02 01:12:03 +0700

    array

--- 39. Arrayforeach/main.js
+++ 39. Arrayforeach/main.js
@@ -1,11 +1,14 @@
-//array.foreach() = experience a provided callback function
-//                  once for each array element
+//array.foreach() = mengalami fungsi panggilan balik yang disediakan
+// satu kali untuk setiap elemen array
 
 let students = ["spongebob", "patrick","squidward"]
 students.forEach(capatilize)
+students.forEach(print) 
 
 function capatilize(element,index,array){
     array[index] = element[0].toUpperCase() + element.substring(1)
 }
 
-console.log(students[0])
+function print(element){
+    console.log(element)
+}
